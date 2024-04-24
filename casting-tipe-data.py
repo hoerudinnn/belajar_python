e6fe082862b94ccd8389fc9ac2ba82fca41d7d48
Hoerudin <hoerudinpermana30@gmail.com>
2024-04-24 22:15:52 +0700

    belajar casting tipe data

---
+++ casting-tipe-data.py
@@ -0,0 +1,59 @@
+# TIPE DATA INTEGER
+print("===INTEGER===")
+data_integer = 30
+print("data: ", data_integer, "bertipe:", type(data_integer))
+
+data_float = float(data_integer)
+data_str = str(data_integer)
+data_bool = bool(data_integer)
+print("data: ", data_float, "bertipe:", type(data_float))
+print("data: ", data_str,   "bertipe:", type(data_str))
+print("data: ", data_bool,  "bertipe:", type(data_bool))
+
+# TIPE DATA FLOAT
+print("===FLOAT===")
+data_float = 5.5
+print("data: ", data_float, "bertipe:", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data: ", data_int,   "bertipe:", type(data_int))
+print("data: ", data_str,   "bertipe:", type(data_str))
+print("data: ", data_bool,  "bertipe:", type(data_bool))
+
+# TIPE DATA FLOAT
+print("===FLOAT===")
+data_float = 5.5
+print("data: ", data_float, "bertipe:", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data: ", data_int,   "bertipe:", type(data_int))
+print("data: ", data_str,   "bertipe:", type(data_str))
+print("data: ", data_bool,  "bertipe:", type(data_bool))
+
+# TIPE DATA STRING
+print("===STRING===")
+data_str = "10"
+print("data: ", data_str, "bertipe:", type(data_str))
+
+data_int = int(data_str)
+data_float = float(data_str)
+data_bool = bool(data_str)
+print("data: ", data_int,   "bertipe:", type(data_int))
+print("data: ", data_float,   "bertipe:", type(data_float))
+print("data: ", data_bool,  "bertipe:", type(data_bool))
+
+# TIPE DATA BOOLEAN
+print("===BOOLEAN===")
+data_bool = False
+print("data: ", data_bool, "bertipe:", type(data_bool))
+
+data_int = int(data_bool)
+data_float = float(data_bool)
+data_str = str(data_bool)
+print("data: ", data_int,   "bertipe:", type(data_int))
+print("data: ", data_float,   "bertipe:", type(data_float))
+print("data: ", data_str,  "bertipe:", type(data_str))
