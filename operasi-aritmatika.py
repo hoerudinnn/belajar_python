ebe36a152ca803230f949323aebe678b0f1b3524
Hoerudin <hoerudinpermana30@gmail.com>
2024-04-24 22:19:16 +0700

    belajar operasi aritmatika

---
+++ operasi-aritmatika.py
@@ -0,0 +1,32 @@
+a = 10
+b = 3
+
+# pertambahan +
+hasil = a + b
+print(a, '+', b, '=', hasil)
+
+# pengurangan -
+hasil = a - b
+print(a, '-', b, '=', hasil)
+
+# perkalian *
+hasil = a * b
+print(a, '*', b, '=', hasil)
+
+# pembagian /
+hasil = a / b
+print(a, '/', b, '=', hasil)
+
+# sisa bagi %
+hasil = a % b
+print(a, '%', b, '=', hasil)
+
+# sisa bagi **
+hasil = a ** b
+print(a, '**', b, '=', hasil)
+
+# sisa bagi //
+hasil = a // b
+print(a, '//', b, '=', hasil)
+
+
