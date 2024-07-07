3e0d42e176ff723ed22f648ef14f86c6fc71d270
Hoerudin <hoerudinpermana30@gmail.com>
2024-07-07 11:23:22 +0700

    menyambung string

---
+++ manipulasi-string.py
@@ -0,0 +1,10 @@
+# 1. menyambung string (concatenate)
+
+nama_pertama = "Ucup"
+nama_kedua = "D"
+nama_ketiga = "Fame"
+
+nama_lengkap = nama_pertama + " " + nama_kedua + "'" + nama_ketiga
+print(nama_lengkap)
+
+# 2. 
