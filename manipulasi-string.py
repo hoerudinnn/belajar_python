ff9ebe1bc9f2b38e285d4cb8801596dd1f79d13a
Hoerudin <hoerudinpermana30@gmail.com>
2024-07-07 14:41:54 +0700

    manipulasi string

--- manipulasi-string.py
+++ manipulasi-string.py
@@ -25,11 +25,11 @@ d = "d"
 status = d not in nama_lengkap
 print(d + " tidak ada di " + nama_lengkap + " = " + str(status))
 
-# 4. mengulang string
+# mengulang string
 print("wk"*10)
 print(20*"wk")
 
-# 5. indexing
+# indexing
 print("index ke-0: " + nama_lengkap[0])
 print("index ke-1: " + nama_lengkap[1])
 print("index ke-(-1): " + nama_lengkap[-1])
@@ -37,3 +37,18 @@ print("index ke-(-2): " + nama_lengkap[-2])
 print("index ke-[0:3]: " + nama_lengkap[0:4])
 print("index ke-[3:7]: " + nama_lengkap[3:8])
 print("index ke-[0,2,4,6,8,10]: " + nama_lengkap[0:11:2])
+
+# nilai paling kecil
+print("index paling kecil:" + min(nama_lengkap))
+# nilai paling besar
+print("index paling besar:" + min(nama_lengkap))
+
+ascii_code = ord(" ")
+print("ASCII code untuk spasi adalah " + str(ascii_code))
+data = 117
+print("char untuk ASCII 117 adalah " + chr(data))
+
+# 4. operator dalam bentuk method
+data = "otong surotong pararotong"
+jumlah = data.count("o")
+print("jumlah o pada " + data + " = " + str(jumlah))
