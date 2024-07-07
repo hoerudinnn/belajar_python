0912d03399d00289694ca8e1a1b8314d32914b32
Hoerudin <hoerudinpermana30@gmail.com>
2024-07-07 11:29:59 +0700

    manipulasi string

--- manipulasi-string.py
+++ manipulasi-string.py
@@ -7,4 +7,8 @@ nama_ketiga = "Fame"
 nama_lengkap = nama_pertama + " " + nama_kedua + "'" + nama_ketiga
 print(nama_lengkap)
 
-# 2. 
+# 2. menghitung panjang
+panjang = len(nama_lengkap)
+print(panjang)
+
+# 3. 
