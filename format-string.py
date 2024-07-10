7431db0e99ff18a1bc2e78ee70be425b651fe0f1
Hoerudin <hoerudinpermana30@gmail.com>
2024-07-10 07:16:20 +0700

    format string

---
+++ format-string.py
@@ -0,0 +1,16 @@
+# contoh generic
+# string
+nama = "hoer"
+format_str = f"Hello, {nama}"
+print(format_str)
+
+boolean = True
+format_str = f"Boolean adalah = {boolean}"
+print(format_str)
+
+# angka 
+angka = 2005.5
+format_str = f"Angka = {angka}"
+print(format_str)
+
+# bilangan bulat
